a595ddc50b48e90f8d78714c88ca92e03248c5dc
Thieres <thieresllima@gmail.com>
2020-10-15 17:59:18 -0300

    Model de agendamento & repositorio typeorm

--- backend/src/database/migrations/1602706692576-CreateAppointments.ts
+++ backend/src/database/migrations/1602706692576-CreateAppointments.ts
@@ -10,7 +10,8 @@ export class CreateAppointments1602706692576 implements MigrationInterface {
             name: 'id',
             type: 'varchar',
             isPrimary: true,
-            generationStrategy: 'uuid'
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
