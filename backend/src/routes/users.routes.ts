9d02d1fbc3f316883763c1e88697bad5c0e5095f
Thieres <thieresllima@gmail.com>
2020-10-20 14:27:01 -0300

    validando credenciais & gerando token JWT

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -6,24 +6,23 @@ const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
   try {
-  const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-  const user = await createUser.execute({
-    name,
-    email,
-    password
-  });
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    });
 
-  user.password = "";
+    user.password = "";
 
-  return response.json(user);
+    return response.json(user);
   }catch (err) {
     return response.status(400).json({ error: err.message});
   };
 
 });
-
 
 export default usersRouter;
