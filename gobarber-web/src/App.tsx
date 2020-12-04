e98bcee54c3e7f6b151b2a3d01c49c4b41600a24
Thieres <thieresllima@gmail.com>
2020-12-04 18:09:52 -0300

    habilitando cors & context API

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Thieres' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
