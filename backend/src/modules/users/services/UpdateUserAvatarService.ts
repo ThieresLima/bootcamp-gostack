44a862a63872649cea436553ac288790a05cbc06
Thieres <thieresllima@gmail.com>
2021-02-09 19:04:29 -0300

    injeção de dependências

--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs';
+import {  injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppErro';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -12,9 +13,13 @@ interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
-class UpdateUserAvatarService {
 
-  constructor(private usersRepository: IUsersRepository) {}
+@injectable()
+class UpdateUserAvatarService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
 
