e22045c707f304dfb1c8067a398276b21c4af945
Thieres <thieresllima@gmail.com>
2021-02-13 20:13:20 -0300

    testes de agendamento

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppErro';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,5 +17,26 @@ describe('CreateAppointmentService', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
+  })
+
+  it('should not be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointmentDate = new Date(2021, 4, 15, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123123'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   })
 });
